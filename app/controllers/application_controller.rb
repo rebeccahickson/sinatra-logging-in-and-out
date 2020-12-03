547007540e9314440bf6f60e7159bf8306239b77
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-03 10:41:59 -0500

    commit tree 384fcb5c989a50e77e9f864c0aed6a11b0ee0960

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    u = User.find_by(username: params[:username], password: params[:password])
+    if u.nil?
+      erb :error
+    else
+      @session = session
+      @session[:user_id] = u.id
+      redirect '/account'
+    end
   end
 
   get '/account' do
-
+    @session = session
+    if Helpers.is_logged_in?(@session)
+      @user = User.find_by(id: @session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
